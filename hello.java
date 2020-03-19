fc240f577e64f8045b9fbd0c2a83c02e46935713
charmisavla <charmipsavla19@gmail.com>
2020-03-19 21:23:01 +0530

    second commit

--- hello.java
+++ hello.java
@@ -1,1 +1,11 @@
-hello charmi
+import java.io.*;
+import java.util.*;
+class hello
+{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+System.out.println("Enter name:");
+String name=s.nextLine();
+System.out.println("\nHello\t"+name);
+}
+}
