8a1674bb913ad9361eea49003ad0f5a1f525b268
charmisavla <charmipsavla19@gmail.com>
2020-03-19 22:42:34 +0530

    commit

--- hello.java
+++ hello.java
@@ -3,9 +3,7 @@ import java.util.*;
 class hello
 {
 public static void main(String args[]){
-Scanner s=new Scanner(System.in);
-System.out.println("Enter name:");
-//String name=s.nextLine();
-System.out.println("\nHello\t");
+for(int i=0;i<10;i++)
+System.out.println("\nHello Charmi");
 }
 }
